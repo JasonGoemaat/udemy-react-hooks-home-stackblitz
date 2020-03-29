8d91e6bcf5f3554619966dc94b4f01545816de53
Jason Goemaat <jason.goemaat@gmail.com>
2020-03-29 18:53:24 -0500

    Finished 6. First Hook: UseState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
+  const handleKeyPress = event => {
+    console.log('handleKeyPress', event);
+    console.log('key:', event.key);
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  };
+
   return (
     <div className="App">
-      App
+      <h1>Hello, world!</h1>
+      <div className="form"></div>
+      <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+      <button onClick={searchQuery}>Search</button>
     </div>
   );
 }
