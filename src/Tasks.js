ec0de83a10adb38d2804b3d4a4c0db6ae9909cb2
Jason Goemaat <jason.goemaat@gmail.com>
2020-03-29 21:51:04 -0500

    Finished Tasks Component Part 2

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import uuid from 'uuid/v4';
+import LocalCache from './LocalCache';
 
 const Tasks = () => {
     const [taskText, setTaskText] = useState('');
-    const [tasks, setTasks] = useState([]);
-    const [completedTasks, setCompletedTasks] = useState([]);
+    const [tasks, setTasks] = useState(LocalCache.get('tasks', []));
+    const [completedTasks, setCompletedTasks] = useState(LocalCache.get('completedTasks', []));
 
     const updateTaskText = event => {
         setTaskText(event.target.value);
@@ -12,10 +13,34 @@ const Tasks = () => {
 
     const addTask = () => {
         const updatedTasks = [...tasks, { taskText, id: uuid() }];
+        LocalCache.set('tasks', updatedTasks);
         setTasks(updatedTasks);
+        setTaskText('');
     }
 
-    console.log('tasks', tasks);
+    const completeTask = completedTask => () => { // second arrow means we return a function, that has completedTask in scope
+        const updatedCompleted = [...completedTasks, completedTask];
+        const updatedTasks = tasks.filter(task => task.id != completedTask.id);
+        setCompletedTasks(updatedCompleted);
+        setTasks(updatedTasks);
+        LocalCache.set('completedTasks', updatedCompleted);
+        LocalCache.set('tasks', updatedTasks);
+    }
+
+    const uncompleteTask = task => () => {
+        const updatedCompleted = completedTasks.filter(x => x.id !== task.id);
+        const updatedTasks = [...tasks, task];
+        setCompletedTasks(updatedCompleted);
+        setTasks(updatedTasks);
+        LocalCache.set('completedTasks', updatedCompleted);
+        LocalCache.set('tasks', updatedTasks);
+    }
+
+    const deleteTask = task => () => {
+        const updatedCompleted = completedTasks.filter(x => x.id !== task.id);
+        setCompletedTasks(updatedCompleted);
+        LocalCache.set('completedTasks', updatedCompleted);
+    }
 
     return (
         <div>
@@ -29,7 +54,23 @@ const Tasks = () => {
                     tasks.map(task => {
                         const { id, taskText } = task;
                         return (
-                            <div key={id}>{taskText}</div>
+                            <div key={id} onClick={completeTask(task)}>{taskText}</div>
+                        )
+                    })
+                }
+            </div>
+            <div className="completed-list">
+                {
+                    completedTasks.map(task => {
+                        const { id, taskText } = task;
+
+                        return (
+                            <div key={id}>
+                                <span onClick={uncompleteTask(task)}>
+                                    {taskText}{' '}
+                                </span>
+                                <span onClick={deleteTask(task)} className='delete-task'>x</span>
+                            </div>
                         )
                     })
                 }
